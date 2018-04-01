994d51380b0137e11f38dbf7cdfcbc0403c7f441
Zhou <540376482yzb@gmail.com>
2018-03-31 17:28:23 -0700

    fix bug number was not generate at start

--- src/components/game.js
+++ src/components/game.js
@@ -2,18 +2,21 @@ import React from 'react'
 import Board from './board'
 import Nav from './nav'
 import './game.css'
-import {initGame} from '../actions'
-import {connect} from 'react-redux'
-export function Game(props){
-	
-	return(
-		<div className='game'>
-			<Nav atClick={e => props.dispatch(initGame(Math.floor(Math.random()*(101))))}/>
-			<h1>COLD OR HOT</h1>
-			<Board/>
-		</div>
-	)
+import { initGame } from '../actions'
+import { connect } from 'react-redux'
+export class Game extends React.Component {
+	componentDidMount() {
+		this.props.dispatch(initGame(Math.floor(Math.random() * 101)))
+	}
+	render() {
+		return (
+			<div className="game">
+				<Nav atClick={e => this.props.dispatch(initGame(Math.floor(Math.random() * 101)))} />
+				<h1>COLD OR HOT</h1>
+				<Board />
+			</div>
+		)
+	}
 }
-
 
 export default connect()(Game)
--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
-import {createStore} from 'redux'
+import { createStore } from 'redux'
 import gameReducer from './reducers/index'
-const store = createStore(gameReducer)
+const store = createStore(gameReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
 export default store
